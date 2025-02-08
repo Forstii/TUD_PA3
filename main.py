e0a273ea1f4d6257efecde96165565e01e0614be
Adrian Forst <adrianforst29@gmail.com>
2025-02-08 13:52:13 +0100

    added dataset

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import datetime
 import os
-
+import numpy
+import pandas
+import matplotlib
+import h5py
+import plotid
 import numpy as np
 
 import project.functions as fn
@@ -12,3 +16,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+print("build finally working")
